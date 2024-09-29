65ea87eff2c37e0dfaae1c9083c354ffc6551d2b
ridzki18 <103101235+ridzki18@users.noreply.github.com>
2024-09-30 05:56:03 +0700

    Update c9.sh

--- c9.sh
+++ c9.sh
@@ -2,8 +2,6 @@ sudo apt-get update && sudo apt-get -y install curl git build-essential
 apt install screen -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
 sudo apt-get -y install nodejs
-git clone https://github.com/c9/core.git sdk
+git clone https://github.com/ridzki18/core9 sdk
 cd sdk
 ./scripts/install-sdk.sh
-screen -S c9
-node server.js -p 8080 -l 0.0.0.0 -a user:passs
